e961bb148b0a4b585d79ff2f751a0e41f2f20918
manojsatya <manoj.pesit@gmail.com>
2021-01-02 15:56:25 +0100

    Created GlobalStyle component

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components/macro';
+
+const GlobalStyle = createGlobalStyle`
+
+
+* {
+    box-sizing: border-box;
+  }
+
+  body {
+      padding: 0px;
+      margin: 0px;
+      /* background: rgb(13, 231, 18); */
+  /* background: linear-gradient(
+    0deg,
+    rgba(13, 231, 18, 1) 17%,
+    rgba(68, 140, 162, 1) 100%
+  ); */
+  }
+`;
+export default GlobalStyle;
