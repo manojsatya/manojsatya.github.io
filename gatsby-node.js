26cd8ddb8ffc3d6b4e9b4abb058f62a8e65ff7ac
manojsatya <manoj.pesit@gmail.com>
2021-01-18 18:19:15 +0100

    Created pages in gastby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,49 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const { slugify } = require('./src/utils/sluglify');
+const path = require('path');
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const slugifyFromTitle = slugify(node.frontmatter.title);
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slugifyFromTitle,
+    });
+  }
+};
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const singleBlogPageTemplate = path.resolve('src/templates/BlogPage.js');
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) return Promise.reject(res.errors);
+
+    const blogs = res.data.allMarkdownRemark.edges;
+
+    blogs.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: singleBlogPageTemplate,
+        context: {
+          slug: node.fields.slug,
+        },
+      });
+    });
+  });
+};
