aaae01775648b215d0bdc8255b5d4bae906387f3
manojsatya <manoj.pesit@gmail.com>
2021-01-19 07:25:32 +0100

    Clean up Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,9 @@
-import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'styled-components';
 
 const Header = () => (
-  <header
-  // style={{
-  //   background: `rebeccapurple`,
-  //   marginBottom: `1.45rem`,
-  // }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <LinkStyled
-          to="/"
-          // style={{
-          //   color: `black`,
-          //   textDecoration: `none`,
-          // }}
-        >
-          Manoj Satyanarayana
-        </LinkStyled>
-      </h1>
-    </div>
+  <header className="bg-indigo-400 w-screen">
+    <h1 className="max-w-screen-lg m-auto">Manoj Satyanarayana</h1>
   </header>
 );
 
@@ -41,8 +16,3 @@ Header.defaultProps = {
 };
 
 export default Header;
-
-const LinkStyled = styled(Link)`
-  color: black;
-  text-decoration: none;
-`;
